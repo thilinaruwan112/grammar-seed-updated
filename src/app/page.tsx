1ec4a81ac4b67dd3fadf28ea2ddc49a468fd11cf
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-09-13 17:58:22 +0000

    remove Why Choose Our Classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from '@/components/home/hero-section';
 import AboutSection from '@/components/home/about-section';
 import ClassesSection from '@/components/home/classes-section';
-import WhyChooseUsSection from '@/components/home/why-choose-us-section';
 import TestimonialsSection from '@/components/home/testimonials-section';
 import NewsSection from '@/components/home/news-section';
 import CtaSection from '@/components/home/cta-section';
@@ -15,7 +14,6 @@ export default function Home() {
       <AboutSection />
       <TeachingApproachSection />
       <StatsSection />
-      <WhyChooseUsSection />
       <ClassesSection />
       <TestimonialsSection />
       <NewsSection />

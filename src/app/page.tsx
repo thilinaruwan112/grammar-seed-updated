ed82f28844d2417b354d78696e9bfcf2020a2e3f
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-09-13 17:59:48 +0000

    add the classess section after the Our Teaching Approach section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <TeachingApproachSection />
+      <ClassesSection />
       <StatsSection />
       <WhyChooseUsSection />
-      <ClassesSection />
       <TestimonialsSection />
       <NewsSection />
       <CtaSection />
